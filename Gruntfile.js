b66af647d57234f7dfc50f44dd06d5f5c9b9fcf9
Gabriel Melo <gabriel@MacBook-Pro-de-Gabriel.local>
2016-12-24 19:03:21 -0300

    Jage for Pug. New Template files

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,36 +7,6 @@ module.exports = function(grunt) {
 
     var packageFile = grunt.file.readJSON('package.json');
 
-    // var mozjpeg = require('imagemin-mozjpeg');
-
-    /*
-     * ------------------------------------------------------
-     * Gera a string com os autores do projeto
-     * ------------------------------------------------------
-     */
-    var authors   = "Gabriel Melo";
-
-    for( i in packageFile.authors ){
-        if( authors == "" ){
-            authors = packageFile.authors[i];
-        }else{
-            authors = authors+"\n\t  "+packageFile.authors[i];
-        }
-    }
-
-    //  Banners dos arquivos
-    var hr = '----------------------------------------------------\n';
-    var bannerFiles = '/*\n' +
-        hr+
-        'Jade App Skeleton\n' +
-        hr+
-        'projeto\t: <%= pkg.name %>\n' +
-        'versao\t: <%= pkg.version %>\n' +
-        'data\t: <%= grunt.template.today("dd/mm/yyyy HH:MM:ss") %>\n' +
-        'autores\t: <%= authors %>\n' +
-        hr+
-         '*/\n';
-
     //  Configurações das Tasks
     grunt.initConfig({
         pkg: packageFile,
